d5b5960384464bc5deec91baac9de99c0120f612
nicferreiramend-arch <nicferreiramend@gmail.com>
2026-02-26 10:51:27 -0300

    Update desafio_1004.py

--- desafio_1004.py
+++ desafio_1004.py
@@ -11,7 +11,12 @@ Saída: Imprima a mensagem "PROD" conforme exemplo abaixo, com um espaço em bra
 antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto,
 caso contrário seu programa apresentará a mensagem: "Presentation Error".
 """
+A = int(input())
+B = int(input())
 
+PROD = A * B
+
+print("PROD = %d" % PROD)
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1004
 
 # Escreva sua solução abaixo
